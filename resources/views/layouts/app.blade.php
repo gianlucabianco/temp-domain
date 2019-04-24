2f9e050aed1f2937fb73b36d898683882b34d021
gianlucabianco <pi.gs.bianco@gmail.com>
2019-04-24 15:16:02 +0200

    init

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href=" {{ asset('css/app.css') }}">
+    <title>Gianluca Bianco, Front End Developer</title>
+  </head>
+  <body>
+    @yield('about')
+  </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('about')
+  @include('partials.about')
+@endsection
